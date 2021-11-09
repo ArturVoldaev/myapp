4c64990934ff90a630a148e99fcddeb6ea884eeb
fishka9044 <79632566+fishka9044@users.noreply.github.com>
2021-11-09 10:48:04 +0300

    myFirtsAppOnReact

---
+++ src/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer style={{background: "#000", color: "#fff"}}>Кто разработал?</footer>
+    ) 
+  }
+}
+
+export default Footer;
---
+++ src/main/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt recusandae illum magni laborum dolorum ratione, pariatur amet deleniti accusantium similique eveniet architecto perferendis illo corporis ducimus molestiae vero quas veniam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt recusandae illum magni laborum dolorum ratione, pariatur amet deleniti accusantium similique eveniet architecto perferendis illo corporis ducimus molestiae vero quas veniam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt recusandae illum magni laborum dolorum ratione, pariatur amet deleniti accusantium similique eveniet architecto perferendis illo corporis ducimus molestiae vero quas veniam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt recusandae illum magni laborum dolorum ratione, pariatur amet deleniti accusantium similique eveniet architecto perferendis illo corporis ducimus molestiae vero quas veniam.
+        </main>
+      </div>
+    ) 
+  }
+}
+
+export default Main;
